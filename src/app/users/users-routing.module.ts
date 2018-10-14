6c800bda0a9b69f906446d9d47c78cf0641a0019
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2018-10-14 22:02:08 +0200

    create route for UsersListingComponent

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersListingComponent } from './users-listing/users-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UsersListingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
