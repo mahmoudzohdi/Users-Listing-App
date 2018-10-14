a9d20088ec57a1ae8e207c50f6d296c65a84b88d
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2018-10-14 22:01:31 +0200

    generate UsersListingComponent

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { UsersListingComponent } from './users-listing/users-listing.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: []
+  declarations: [UsersListingComponent]
 })
 export class UsersModule { }
