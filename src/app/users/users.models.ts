062d6830a9bba790a3d44fb20fdf1fbb5c743ce3
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2018-10-14 22:24:20 +0200

    create users service & models

---
+++ src/app/users/users.models.ts
@@ -0,0 +1,19 @@
+interface GeoLocation {
+  lat: string,
+  lng: string
+}
+interface Address {
+  street: string,
+  suite: string,
+  city: string,
+  zipcode: string,
+  geo: GeoLocation
+}
+export interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: Address,
+  phone: string
+}
