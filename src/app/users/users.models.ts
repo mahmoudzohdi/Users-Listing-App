b30672dbf4aef088f7acb369081276dfa2588d8f
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2018-10-14 23:05:30 +0200

    change db.json and update User interface

--- src/app/users/users.models.ts
+++ src/app/users/users.models.ts
@@ -1,19 +1,8 @@
-interface GeoLocation {
-  lat: string,
-  lng: string
-}
-interface Address {
-  street: string,
-  suite: string,
-  city: string,
-  zipcode: string,
-  geo: GeoLocation
-}
 export interface User {
   id: number,
   name: string,
   username: string,
   email: string,
-  address: Address,
+  address: string,
   phone: string
 }
