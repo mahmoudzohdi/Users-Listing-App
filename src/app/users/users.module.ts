f4a602dbcb8490b9fe9c09a7cfc4b07a1a234fc8
Mahmoud Zohdi <mahmoud.zohdi@gmail.com>
2018-10-14 23:25:08 +0200

    create userFormComponent to be add/edit forms for user

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersListingComponent } from './users-listing/users-listing.component';
 import { UsersService } from './users.service';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: [UsersListingComponent],
+  declarations: [UsersListingComponent, UserFormComponent],
   providers: [
     UsersService
   ]
